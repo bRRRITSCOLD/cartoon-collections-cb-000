3f4cf21fd0142d33b6275f0973d2ad759be32064
Blaine Richardson <blainerrichardson@gmail.com>
2017-09-21 14:55:20 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(dwarves)
   dwarves.each.with_index(1) do |dwarf, index|
     puts "#{index}. #{dwarf}"
+  end
 end
 
 def summon_captain_planet(planeteer_calls)
@@ -12,8 +13,9 @@ def long_planeteer_calls(planeteer_calls)
 end
 
 def find_the_cheese(potentially_cheesy_items)
-    cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses = ["gouda", "cheddar", "camembert"]
 
-    potentially_cheesy_items.find do |maybe_cheese|
-      cheeses.include?(maybe_cheese)
+  potentially_cheesy_items.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
+  end
 end
